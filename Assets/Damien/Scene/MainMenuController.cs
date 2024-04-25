46cfb94c9d664d45a73f65ecd4d0914145e0e95a
Landon J Perkin <ljperkin@my.okcu.edu>
2024-04-25 09:37:58 -0500

    Functional main menu

--- Assets/Damien/Scene/MainMenuController.cs
+++ Assets/Damien/Scene/MainMenuController.cs
@@ -10,15 +10,15 @@ public class MainMenuController : MonoBehaviour
    }
 
    public void Level1(){
-
+        SceneManager.LoadScene(1);
    }
 
    public void Level2(){
-    
+        SceneManager.LoadScene(2);
    }
 
    public void Level3(){
-    
+        SceneManager.LoadScene(3);
    }
 
    public void Level4(){
